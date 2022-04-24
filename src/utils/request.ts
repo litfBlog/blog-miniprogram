d51e8c2767f4d0fd31840946437878953af4a7bc
Litfa <admin@litf.com.cn>
2022-04-24 19:57:57 +0800

    feat: 请求携带token

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,23 +1,31 @@
 /*
  * @Author: litfa
  * @Date: 2022-03-03 10:09:26
- * @LastEditTime: 2022-04-17 17:16:51
+ * @LastEditTime: 2022-04-24 19:57:18
  * @LastEditors: litfa
  * @Description: http
  * @FilePath: /blog-miniprogram/src/utils/request.ts
  * 
  */
+const base_url = import.meta.env.VITE_ApiUrl as string + import.meta.env.VITE_baseUrl as string
+
 interface config {
   url?: string
   method?: "GET" | "OPTIONS" | "HEAD" | "POST" | "PUT" | "DELETE" | "TRACE" | "CONNECT" | undefined
   data?: object
 }
 
-// const base_url = 'http://127.0.0.1:3000/api'
-const base_url = import.meta.env.VITE_ApiUrl as string + import.meta.env.VITE_baseUrl as string
 export default (config: config) => {
   return new Promise((resolve, reject) => {
+    let token
+    try {
+      token = uni.getStorageSync('token')
+    } catch (e) { }
+
     uni.request({
+      header: {
+        'Authorization': token
+      },
       url: base_url + config.url || base_url,
       method: config.method || 'GET',
       data: config.data,
