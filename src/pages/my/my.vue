57560e53af72c714ad27dccabf32783aae7b1c3c
Litfa <admin@litf.com.cn>
2022-04-24 19:19:24 +0800

    feat: 个人页

--- src/pages/my/my.vue
+++ src/pages/my/my.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: litfa
  * @Date: 2022-03-02 19:03:56
- * @LastEditTime: 2022-03-02 20:00:47
+ * @LastEditTime: 2022-04-24 19:16:12
  * @LastEditors: litfa
  * @Description: 
  * @FilePath: /blog-miniprogram/src/pages/my/my.vue
@@ -16,8 +16,32 @@ const toLogin = () => {
 </script>
 
 <template>
-  <button @click="toLogin">登录</button>
+  <div class="header" @click="toLogin">
+    <div class="content">
+      <div class="user">
+        <div class="avatar"></div>
+        <span class="username">登录/注册</span>
+      </div>
+      <div class="data">
+        <div class="item">
+          <span class="count">0</span>
+          <span class="text">粉丝</span>
+        </div>
+        <div class="item">
+          <span class="count">0</span>
+          <span class="text">获赞</span>
+        </div>
+        <div class="item">
+          <span class="count">0</span>
+          <span class="text">文章</span>
+        </div>
+      </div>
+    </div>
+    <div class="container"></div>
+  </div>
+  <!-- <button @click="toLogin">登录</button> -->
 </template>
 
-<style scoped>
+<style lang="less" scoped>
+@import "./my.less";
 </style>
